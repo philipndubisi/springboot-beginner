d1c829e692a170a68cb936a86dc31848ed2de557
Philip <philipndubisi93@gmail.com>
2024-11-12 14:26:50 +0100

    Spring web MVC

--- src/main/java/com/prework/Main.java
+++ src/main/java/com/prework/Main.java
@@ -2,17 +2,21 @@ package com.prework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Controller
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/greetings")
-    public String greet(){
-        return "Hello";
+    @GetMapping(value="/greetings")
+    public GreetResponse greet(){
+        return new GreetResponse("Hello");
     }
+
+    record GreetResponse(String greet){}
 }
